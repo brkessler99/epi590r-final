c18e94ebceebb77c6218ab7c1de8fb0c8929b2c2
brk1112 <benjamin.kessler@emory.edu>
2023-08-21 17:58:50 -0400

    commit-check

--- number-3.R
+++ number-3.R
@@ -1,3 +1,7 @@
+pacman::p_load(tidyverse,
+               gtsummary,
+               patchwork)
+
 #big mac data from https://github.com/TheEconomist/big-mac-data 
 
 bigmac <- rio::import(here::here("data","raw","big-mac-2022-07-01.xls")) %>% janitor::clean_names()
@@ -8,14 +12,50 @@ regions <- rio::import(here::here("data","raw","world-regions-sdg-united-nations
   janitor::clean_names() %>% 
   select(code,world_regions_according_to_the_united_nations) %>% 
   rename("iso_a3"="code","region"="world_regions_according_to_the_united_nations")
-bigmac2 <- left_join(bigmac,regions)
+bigmac2 <- left_join(bigmac,regions) %>% drop_na()
 
-bigmac <- bigmac %>% mutate(above_us = case_when(
-  bigmac$dollar_price >= bigmac$dollar_price[bigmac$Country == "United States"] ~ 1,
-  TRUE ~ 0
-))
+#summary statistics
+tbl_summary(bigmac2,
+            by=region,
+            include=c(local_price, dollar_price, dollar_ppp, gdp_bigmac, dollar_valuation),
+            label=list(local_price ~ "Local Price",
+                       dollar_price ~ "Dollar Price",
+                       dollar_ppp ~ "USD PPP",
+                       gdp_bigmac ~ "Big Mac GDP",
+                       dollar_valuation ~ "Dollar Valuation")) %>% 
+  add_overall(col_label = "**Total**")
 
-#create world region var
-#table with local_price, dollar_price, dollar_ppp, GDP_bigmac, dollar_valuation
+#regression statistics
+model <- lm(gdp_bigmac ~ local_price + dollar_ex, data=bigmac2)
+tbl_regression(model,
+               intercept = TRUE,
+               label = list(
+                 local_price ~ "Local Price",
+                 dollar_ex ~ "Dollar Exchange Rate"
+               ))
 
-tbl_summary(bigmac2,by=region,include=c(local_price, dollar_price, dollar_ppp, gdp_bigmac, dollar_valuation))
+#create a figure
+price_graph <- bigmac2 %>% ggplot(aes(x=log(local_price),y=log(gdp_bigmac))) + 
+  geom_point(color="red") +
+  labs(x="log(Local Price)",y="log(Dollar Echange Rate)")
+dollar_graph <- bigmac2 %>% ggplot(aes(x=log(dollar_ex),y=log(gdp_bigmac))) + 
+  geom_point(color="blue") +
+  labs(x="log(Local Price)",y="log(Dollar Echange Rate)")
+price_graph + dollar_graph
+
+#create a function
+    #input iso_a3 for geometric mean of local prices
+
+geo_mean <- function(x) {
+  df <- data.frame(matrix(nrow=length(x),ncol=2))
+  colnames(df) <- c("position","local_price")
+  for (i in 1:length(x)) {
+    df[i,1] <- which(bigmac2$iso_a3 == x[i])
+    df[i,2] <- bigmac2$local_price[df[i,1]]
+  }
+  n <- length(x)
+  geo_mean <- (prod(df$local_price))^(1/n)
+  return(geo_mean)
+}
+
+geo_mean(x=c("VNM","URY","AUS","THA","KWT"))
